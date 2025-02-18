a83e201443499b7bda425c74b6e3a279e0bb9d32
kyuceak <kyuceak@sabanciuniv.edu>
2025-02-19 00:57:11 +0300

    swiper setup implemented

---
+++ src/components/product-card/product-card.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./product-card.css";
+import StarRating from "../star-rating";
+
+function ProductCard({ productName, productImages }) {
+  const [selectedColor, setSelectedColor] = useState(1);
+
+  return (
+    <>
+      <div className="card">
+        <img src={productImages.yellow} alt="" className="image-card" />
+        <div className="card-content">
+          <h3 className="card-title">{productName}</h3>
+          <p className="card-price">$150USD</p>
+          <div className="colors">
+            <div
+              className={
+                selectedColor == 1
+                  ? "circle color-1 selected"
+                  : "circle color-1"
+              }
+              onClick={() => setSelectedColor(1)}
+            ></div>
+            <div
+              className={
+                selectedColor == 2
+                  ? "circle color-2 selected"
+                  : "circle color-2"
+              }
+              onClick={() => setSelectedColor(2)}
+            ></div>
+            <div
+              className={
+                selectedColor == 3
+                  ? "circle color-3 selected"
+                  : "circle color-3"
+              }
+              onClick={() => setSelectedColor(3)}
+            ></div>
+          </div>
+          <p className="selected-color"></p>
+          <StarRating />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductCard;
---
+++ src/components/star-rating.jsx
@@ -0,0 +1,11 @@
+
+
+
+function StarRating(){
+
+
+
+    return <></>
+}
+
+export default StarRating;
